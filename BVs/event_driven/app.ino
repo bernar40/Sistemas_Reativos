271deaa0bdf713a72bba912874231efd4da412f1
bernar40 <bernardo.ruga@hotmail.com>
2018-03-18 16:14:00 -0300

    Quase, so falta parar de contar 2x

--- BVs/event_driven/app.ino
+++ BVs/event_driven/app.ino
@@ -2,19 +2,65 @@
 #include "app.h"
 #include "pindefs.h"
 
+static bool LED = 1;
+static unsigned long time2_interval = 1000;
+static unsigned long bt1_before = 0;
+static unsigned long bt2_before = 0;
+
 void appinit(void) {
-  button_listen(KEY1);
-  button_listen(KEY2);
-  timer_set(1000);
+  button_listen(KEY1); //interessado no botao 1
+  button_listen(KEY2); //interessado no botao 2
+  timer_set(1000); //intervalo inicial
+  Serial.begin(9600);
+
 }
 void button_changed(int p, int v) {
-  if (p == KEY1){
-    digitalWrite(LED1, v);
-    digitalWrite(LED2, v);
-    digitalWrite(LED3, v);
-  }
+  unsigned long current = millis(); //contabilizando tempo
+
+    if (p == KEY1){ //Botao 1 foi pressionado ou liberado
+    	if(v == 0){ //Botao 1 -> pressionado
+        	bt1_before = current; //comeca a contar timer
+        }
+	    else{
+	      	if(current - bt2_before < 500){ //botoes apertados "ao mesmo tempo" -> led desliga
+            Serial.println("1 Desliga");
+		        digitalWrite(LED1, HIGH);
+		        timer_set(0);
+	      	}
+	      	else{ //apenas botao 1 -> led fica mais rapido
+		        Serial.println("Fica mais rapido");
+		        time2_interval = time2_interval - 200;
+		        if (time2_interval < 200){
+		        	time2_interval = 200;
+		        }
+		        timer_set(time2_interval);
+      		}
+    	}
+    }
+//  	if (p == KEY2){ //Botao 2 foi pressionado ou liberado
+      else{
+        	if(v == 0){ //Botao 2 -> pressionado
+          		bt2_before = current;
+          	}
+        	else{
+    	      	if(current - bt1_before < 500){//botoes apertados "ao mesmo tempo" -> led desliga
+                Serial.println("2 Desliga");
+    		       	digitalWrite(LED1, HIGH);
+    		        timer_set(0);
+    	      	}
+    	      	else{ // apenas botao 2 -> led fica mais devagar
+    		        Serial.println("Fica mais devagar");
+    		        time2_interval = time2_interval+200;
+    		        timer_set(time2_interval);
+          		}
+        	}
+    	}
 }
 void timer_expired(void) {
-  digitalWrite(LED1, 0);
-  timer_set(1000);
+  if(LED == 1)
+    digitalWrite(LED1, LOW);
+  else
+    digitalWrite(LED1, HIGH);
+  LED = !LED;
 }
+
--- BVs/event_driven/event_driven.ino
+++ BVs/event_driven/event_driven.ino
@@ -2,10 +2,15 @@
 #include "app.h"
 #include "pindefs.h"
 
-static bool button1_interess = 1;
-static bool button2_interess = 1;
-static bool button1_active = 0;
-static bool button2_active = 0;
+static bool button1_interess = 0;
+static bool button2_interess = 0;
+static bool button1_active = 1;
+static bool button2_active = 1;
+static unsigned long time_interval = 0;
+static unsigned long time_now;
+static unsigned long time_before = 0;
+bool button1_read;
+bool button2_read;
 
 void button_listen (int pin) {
    if (pin == KEY1)
@@ -13,37 +18,40 @@ void button_listen (int pin) {
    if (pin == KEY2)
      button2_interess = 1;
 }
-void timer_set (int ms) {
-
-  // timer deve expirar após “ms” milisegundos
+void timer_set(int ms){
+ time_interval = ms; 
 }
 
 /*Codigo Principal*/
 void setup() {
+  pinMode(LED1, OUTPUT);
   pinMode(KEY1, INPUT_PULLUP);
   pinMode(KEY2, INPUT_PULLUP);
-  Serial.begin(9600);
-  //appinit();
+  appinit();
 }
 
 void loop() {
-  digitalWrite(LED3, HIGH);
-  delay(200);
-  digitalWrite(LED3, LOW);
-  delay(200);
-  bool button1_read;
-  bool button2_read;
-    
-  if (button1_interess){
-    button1_read = digitalRead(KEY1);
-    if (button1_active != button1_read) {
-      Serial.println("entrou no 2o if");
-      Serial.println(button1_read);
-
-      button_changed(KEY1, !button1_read);
+  if (button1_interess){ //interessado no botao 1
+    button1_read = digitalRead(KEY1); //le o botao 1
+    if (button1_read != button1_active) { //vai ativar quando for precionado ou levantado
+      button_changed(KEY1, button1_read);
       button1_active = button1_read;
     }
   }
-
+  if (button2_interess){ //interessado no botao 2
+    button2_read = digitalRead(KEY2); //le o botao 2
+    if (button2_read != button2_active) { //vai ativar quando for precionado ou levantado
+      button_changed(KEY2, button2_read);
+      button2_active = button2_read;
+    }
+  }
+  if(time_interval){
+    time_now = millis();
+    if(time_now - time_before > time_interval){
+     timer_expired();
+     time_before = time_now; 
+    }
+  }
 }
+
 
