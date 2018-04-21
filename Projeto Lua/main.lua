56330928305f28c63a75b5664df55788ebe8bbec
Bernardo Ruga <bernardo.ruga@hotmail.com>
2018-04-20 21:25:50 -0300

    Add files via upload

--- Projeto Lua/main.lua
+++ Projeto Lua/main.lua
@@ -4,45 +4,51 @@ function wait (sec, objeto)
   coroutine.yield()
 end
 
-function inimigo (x1,y1,x2,y2,x3,y3, vel, enemies)
-  table.insert(enemies, {
-      px1 = x1,
-      py1 = y1,
-      px2 = x2,
-      py2 = y2,
-      px3 = x3,
-      py3 = y3,
-      hitbox_px = x1 - (x2-x1)/2,
-      hitbox_py = y1 - (y3-y1)/2,
-      width = (x2-x1),
-      height = (y3-y1),
-      is_hit = 0
-    })
+function inimigo (x1,y1,x2,y2,x3,y3, vel)
+      local px1 = x1
+      local py1 = y1
+      local px2 = x2
+      local py2 = y2
+      local px3 = x3
+      local py3 = y3
+      local hitbox_px = x1 - (x2-x1)/2
+      local hitbox_py = y1 - (y3-y1)/2
+      local width = (x2-x1)
+      local height = (y3-y1)
+      local is_hit = 0
   return {
-    update = coroutine.wrap (function (self, dt, enemy)
+    update = coroutine.wrap (function (self, dt)
       local width, height = love.graphics.getDimensions()
       while true do
  --       for n,enemy in ipairs(enemies) do
-          enemy.px1 = enemy.px1 + vel*dt
-          enemy.px2 = enemy.px2 + vel*dt
-          enemy.px3 = enemy.px3 + vel*dt
-          if enemy.px1 > width then 
-            enemy.px1 = x1
-            enemy.px2 = x2
-            enemy.px3 = x3
+          px1 = px1 + vel*dt
+          px2 = px2 + vel*dt
+          px3 = px3 + vel*dt
+          if px1 > width then 
+            px1 = x1
+            px2 = x2
+            px3 = x3
           end
-        enemy.hitbox_px = (enemy.px1- (enemy.px2-enemy.px1)/2) + vel*dt
+        hitbox_px = (px1- (px2-px1)/2) + vel*dt
         wait(vel/1000, self)
- --       end
       end
     end),
+    collisao = 
+      function()
+        for n, missil in ipairs(misseis) do
+          if checar_colisao(missil.hitbox_px, missil.hitbox_py, missil.width, missil.height, hitbox_px, hitbox_py,                            width, height) then
+            is_hit = 1
+            table.remove(inimigos, n)
+            table.remove(misseis, i)
+            break
+          end
+        end
+      end,
     draw =
       function ()
-        for i, enemy in ipairs(enemies) do
-          if enemy.is_hit == 0 then
-            love.graphics.setColor (255,0,0)
-            love.graphics.polygon("fill", enemy.px1, enemy.py1, enemy.px2, enemy.py2, enemy.px3, enemy.py3)
-          end
+        if is_hit == 0 then
+          love.graphics.setColor (255,0,0)
+          love.graphics.polygon("fill", px1, py1, px2, py2, px3, py3)
         end
       end
   }
@@ -107,7 +113,7 @@ function love.load()
   is_hit = {}
   player = player(player_x, player_y, 25, 25)
   for i=1, 5 do
-    inimigos[i] = inimigo(100+(i*100), 100, 125+(i*100), 100, 112.5+(i*100), 125, 500, enemies)
+    inimigos[i] = inimigo(100+(i*100), 100, 125+(i*100), 100, 112.5+(i*100), 125, 500)
     is_hit[i] = 0
   end
 end
@@ -119,10 +125,9 @@ end
 function love.update(dt)
   current_time = love.timer.getTime()
   player.update(dt)
-  for n, enemy in ipairs(enemies) do
-    if enemy.is_hit == 0 then
-      inimigos[n].update(inimigos[n], dt, enemy)
-    end
+  for n, inimigo in ipairs(inimigos) do
+      inimigos[n].update(inimigos[n], dt)
+      inimigos[n].collisao()
   end
   for i, missil in ipairs(misseis) do
     missil_move(dt, missil)
@@ -130,17 +135,6 @@ function love.update(dt)
   for i, missil in ipairs(misseis) do
     if missil.py < 0 then -- remocao de misseis--     
       table.remove(misseis, i)
-    else
-      for n, enemy in ipairs(enemies) do
-        if checar_colisao(missil.hitbox_px, missil.hitbox_py, missil.width, missil.height, enemy.hitbox_px, enemy.hitbox_py, enemy.width, enemy.height) then
---          table.remove(inimigos, n)
---          is_hit[n] = 1
-          enemy.is_hit = 1
-          table.remove(enemies, n)
-          table.remove(misseis, i)
-          break
-        end
-      end
     end
   end
 end
@@ -149,10 +143,8 @@ function love.draw()
   love.graphics.setColor (0,255,0)
   love.graphics.rectangle("fill", 0, 575, 800, 25)
   player.draw()
-  for n, enemy in ipairs(enemies) do
-    if enemy.is_hit == 0 then
+  for n, inimigo in ipairs(inimigos) do
       inimigos[n].draw()
-    end
   end
   for i, missil in ipairs(misseis) do
     love.graphics.setColor (255,255,0)
