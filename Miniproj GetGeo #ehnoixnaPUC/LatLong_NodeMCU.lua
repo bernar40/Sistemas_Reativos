f4e8455ddf06a3bea2fd48d872ae10c13ccea463
bernar40 <bernardo.ruga@hotmail.com>
2018-05-23 16:40:36 -0300

    100%, so falta o API querer funcionar hahahaha

---
+++ Miniproj GetGeo #ehnoixnaPUC/LatLong_NodeMCU.lua
@@ -0,0 +1,120 @@
+sw1 = 1
+led_r = 3
+led_g = 6
+gpio.mode(led_g, gpio.OUTPUT)
+gpio.mode(led_r, gpio.OUTPUT)
+gpio.mode(sw1,gpio.INT,gpio.PULLUP)
+
+local m = mqtt.Client("1511651", 120)
+
+function publica(c, localizacaoMsg)
+  pub = c:publish("latlong", localizacaoMsg ,0,0, 
+            function(client, reason) print("localizacao enviada!") end)
+  if pub == 1 then
+    print("Localizacao publicada")
+  else
+    print("Erro na publicacao")
+  end
+end
+
+
+function Lovecallback(c)
+    local function mensagemLove(userdata, topic, message)
+            if(message == "Pedido de localizacao") then
+                gpio.write(led_r, gpio.HIGH)
+                wifi.sta.getap(listap)
+            end
+    end
+    c:on("message",mensagemLove)
+end
+
+
+function conectado (c)
+    c:publish("Sucesso", "NodeMCU Conectou.", 0,  0, 
+                  function(client, reason) print("Conexão estabelecida") end)
+    c:subscribe("PedidoLove", 0, Lovecallback)
+end 
+
+function Parse_Data(data)
+      local i, j = string.find(data, '"lat": ')
+      local v1 = string.find(data, ',')
+      local lat = string.sub(data, j+1, v1-1)
+      i, j = string.find(data, '"lng": ')
+      local v2 = string.find(data, ',', v1+1)
+      local long = string.sub(data, j+1, v2-3)
+      localizacaoMsg = lat.. ' '..long
+      return localizacaoMsg
+  end
+
+-- Print AP list that is easier to read
+function listap(t) -- (SSID : Authmode, RSSI, BSSID, Channel)
+    local table_macs = {}
+    local table_ss = {}
+    local table_channels = {}
+--    print("\n"..string.format("%32s","SSID").."\tBSSID\t\t\t\t  RSSI\t\tAUTHMODE\tCHANNEL")
+    wifi_ap = [[{ "wifiAccessPoints":[]]
+    for ssid,v in pairs(t) do
+        local authmode, rssi, bssid, channel = string.match(v, "([^,]+),([^,]+),([^,]+),([^,]+)")
+--        print(string.format("%32s",ssid).."\t"..bssid.."\t  "..rssi.."\t\t"..authmode.."\t\t\t"..channel)
+        table.insert(table_macs, bssid)
+        table.insert(table_ss, rssi)
+        table.insert(table_channels, channel)
+    end
+    for i = 1,#table_macs do
+        mac_adress = [[{ "macAddress": "]] .. table_macs[i] .. [[", ]]
+        ss = [["signalStrength": ]] .. table_ss[i] .. [[, ]]
+        channel = [["channel": ]] .. table_channels[i] .. [[}]]
+        mac_adress = mac_adress .. ss
+        mac_adress = mac_adress .. channel
+        if i < #table_macs then
+           mac_adress = mac_adress .. [[,]]
+        end
+        wifi_ap = wifi_ap .. mac_adress
+        i = i + 1
+    end
+    end_ap =[[] }]]
+    wifi_ap = wifi_ap .. end_ap
+    
+    print(wifi_ap)
+    
+    local tentativas = 5
+    local function callbackPost(code,data)
+            if (code < 0) or (code == 411) then
+                print("HTTP request failed :", code)
+                tentativas = tentativas - 1
+                print("Numeros de tentativas restantes: " .. tentativas)
+                if(tentativas > 0) then
+                   http.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAyaHaY12hoag35S-Tdy4WD5XxT5WsprPY',
+                    'Content-Type: application/json\r\n',wifi_ap,callbackPost)
+                end
+            else
+                print(code, data)
+                localizacaoMsg = Parse_Data(data)
+                publica(m, localizacaoMsg)
+            end
+    end
+
+    http.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAyaHaY12hoag35S-Tdy4WD5XxT5WsprPY',
+      'Content-Type: application/json\r\n',wifi_ap,callbackPost)
+      gpio.write(led_g, gpio.LOW)
+      gpio.write(led_r, gpio.LOW)
+end
+
+function buttonpressed()
+    local delay = 500000
+    local last = 0
+    return
+    function (level, timestamp)
+        local now = tmr.now()
+        if now - last < delay then return end
+            last = now
+            gpio.write(led_g, gpio.HIGH)
+            wifi.sta.getap(listap)
+    end
+end
+gpio.trig(sw1, "down", buttonpressed())
+
+m:connect("test.mosquitto.org", 1883, 0, 
+             conectado,
+             function(client, reason) print("failed reason: "..reason) end)
+
--- Miniproj GetGeo #ehnoixnaPUC/main.lua
+++ Miniproj GetGeo #ehnoixnaPUC/main.lua
@@ -1,19 +1,35 @@
 local mqtt = require("mqtt_library")
+love.window.setTitle("NodeMCU LatLong - Bernardo & Alexandre")
 
-function mqttcb(topic, message)
-   print("Received from topic: " .. topic .. " - message:" .. message)
-   space = string.find(message, " ")
-   lat = string.sub(message, 1, space1-1)
-   long = string.sub(message, space1+1, string.len(message))
-   print(lat)
-   print(long)
+function mqttcb (topic, message)
+  print("Received from topic: " .. topic .. " - message:" .. message)
+  space = string.find(message, " ")
+  lat = string.sub(message, 1, space1-1)
+  long = string.sub(message, space1+1, string.len(message))
+  recieved = 1
 end
 
+
+
 function love.load()
-  controle = false
+  lat = 0
+  long = 0
+  
+  w = 300
+  h = 300
+  love.window.setMode(w,h)
+  
   m = mqtt.client.create("test.mosquitto.org", 1883, mqttcb)
-  m:connect("1511651")
+  m:connect("BernardoSnow")
   m:subscribe({"latlong"})
+end
+
+function love.keypressed(key)
+  if key == 'space' then
+    lat = 0
+    long = 0
+    m:publish("PedidoLove", "Pedido de localizacao")
+  end
 end
 
 function love.update (dt)
@@ -21,8 +37,18 @@ function love.update (dt)
 end
 
 function love.draw ()
-    if controle then
-      love.graphics.rectangle("fill", 20, 50, 60, 120 ) 
-    end
+  love.graphics.setColor(0, 0, 255)
+  love.graphics.rectangle('line', 40, 30, 220, 60)
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.print("NodeMCU Localizacao!", 55, 50, 0, 1.4, 1.4)
+  love.graphics.print("Latitude: ", 10, 140, 0, 1.4, 1.4)
+  love.graphics.print("Longitude: ", 10, 160, 0, 1.4, 1.4)
   
+  if recieved == 1 then
+    love.graphics.print(lat, 95, 140, 0, 1.4, 1.4)
+    love.graphics.print(long, 110, 160, 0, 1.4, 1.4)
+  else
+    love.graphics.print("xxxxxxx", 95, 140, 0, 1.4, 1.4)
+    love.graphics.print("xxxxxxx", 110, 160, 0, 1.4, 1.4)
+  end
 end
