51a0c6e51a366b10435ffdd61356e9ed2a8e67dc
Bernardo Ruga <bernardo.ruga@hotmail.com>
2018-05-22 23:03:33 -0300

    roda esse sem os comentarios e a parte de cima

--- Miniproj GetGeo #ehnoixnaPUC/main.lua
+++ Miniproj GetGeo #ehnoixnaPUC/main.lua
@@ -1,86 +1,35 @@
-mqtt = require("mqtt_library")
+local mqtt = require("mqtt_library")
 
-wificonf = {
-  -- verificar ssid e senha
-  ssid = "reativos",
-  pwd = "reativos",
-  save = false
-}
-
-function publica(c)
-  c:publish("alos","alo do nodemcu!",0,0, 
-            function(client) print("mandou!") end)
+function mqttcb(topic, message)
+   print("Received from topic: " .. topic .. " - message:" .. message)
+   controle = not controle
 end
 
-function conectado (client)
-  publica(client)
-  client:subscribe("alos", 0, novaInscricao)
+function love.keypressed(key)
+  if key == 'a' then
+    mqtt_client:publish("apertou-tecla", "a")
+  end
 end
 
 function love.load()
   controle = false
-  --mqtt_client = mqtt.client.create("test.mosquitto.org", 1883, mqttcb)
-  --mqtt_client:connect("1511651")
-  --mqtt_client:subscribe({"apertou-tecla"})
-  ret1 = retangulo(50, 200, 200, 150)
-  
-  wifi.sta.config(wificonf)
-  print("modo: ".. wifi.setmode(wifi.STATION))
-  print(wifi.sta.getip())
-  
-  sw1 = 1
-  gpio.mode(sw1,gpio.INT,gpio.PULLUP)
-  m = mqtt.Client("test.mosquitto.org", 120)
-  m:connect("1511651", 1883, mqttcb, 
-             conectado,
-             function(client, reason) print("failed reason: "..reason) end)
-  
+  mqtt_client = mqtt.client.create("test.mosquitto.org", 1883, mqttcb)
+  mqtt_client:connect("cliente love 1")
+  mqtt_client:subscribe({"apertou-tecla"})
 end
 
--- Print AP list that is easier to read
-function listap(t) -- (SSID : Authmode, RSSI, BSSID, Channel)
-    local table_macs = {}
-    local table_ss = {}
-    local table_channels = {}
---    print("\n"..string.format("%32s","SSID").."\tBSSID\t\t\t\t  RSSI\t\tAUTHMODE\tCHANNEL")
-    wifi_ap = [[{ "wifiAccessPoints":[]]
-    for ssid,v in pairs(t) do
-        local authmode, rssi, bssid, channel = string.match(v, "([^,]+),([^,]+),([^,]+),([^,]+)")
---        print(string.format("%32s",ssid).."\t"..bssid.."\t  "..rssi.."\t\t"..authmode.."\t\t\t"..channel)
-        table.insert(table_macs, bssid)
-        table.insert(table_ss, rssi)
-        table.insert(table_channels, channel)
-    end
-    for i = 1,#table_macs do
-        mac_adress = [[{ "macAddress": "]] .. table_macs[i] .. [[", ]]
-        ss = [["signalStrength": ]] .. table_ss[i] .. [[, ]]
-        channel = [["channel": ]] .. table_channels[i] .. [[}]]
-        mac_adress = mac_adress .. ss
-        mac_adress = mac_adress .. channel
-        if i < #table_macs then
-           mac_adress = mac_adress .. [[,]]
-        end
-        wifi_ap = wifi_ap .. mac_adress
-        i = i + 1
-    end
-    end_ap =[[] }]]
-    wifi_ap = wifi_ap .. end_ap
-    --print(wifi_ap)
+function love.draw()
+   if controle then
+     love.graphics.rectangle("line", 10, 10, 200, 150)
+   end
 end
 
-gpio.trig(sw1, "down", wifi.sta.getap(listap))
---print(wifi_ap)
+function love.update(dt)
+  mqtt_client:handler()
+end
+  
 
-http.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAyaHaY12hoag35S-Tdy4WD5XxT5WsprPY',
-  'Content-Type: application/json\r\n',
-  wifi_ap,
-  function(code, data)
-    if (code < 0) then
-      print("HTTP request failed :", code)
-    else
-      print(code, data)
-    end
-  end)
+--[[mqtt = require("mqtt_library")
 
 function mqttcb(topic, message)
    print("Received from topic: " .. topic .. " - message:" .. message)
@@ -90,6 +39,33 @@ function mqttcb(topic, message)
    local_x = string.sub(message, space1+1, space2-1)
    local_y = string.sub(message, space2+1)
    controle = not controle
+end
+
+function publica(c)
+  c:publish("alos","alo do nodemcu!",0,0, 
+            function(client) print("mandou!") end)
+end
+
+function novaInscricao (c)
+  local msgsrec = 0
+  function novamsg (c, t, m)
+    print ("mensagem ".. msgsrec .. ", topico: ".. t .. ", dados: " .. m)
+    msgsrec = msgsrec + 1
+  end
+  c:on("message", novamsg)
+end
+
+function conectado (client)
+  print("Cliente conectado!")
+end
+
+function love.load()
+  controle = false
+  ret1 = retangulo(50, 200, 200, 150)
+  m = mqtt.Client("1511651", 120)
+  m:connect("test.mosquitto.org", 1883, mqttcb, 
+             conectado,
+             function(client, reason) print("failed reason: "..reason) end)
 end
 
 function naimagem (mx, my, x, y) 
@@ -126,13 +102,4 @@ function love.draw ()
       ret1.draw()
     end
   
-end
-
-function novaInscricao (c)
-  local msgsrec = 0
-  function novamsg (c, t, m)
-    print ("mensagem ".. msgsrec .. ", topico: ".. t .. ", dados: " .. m)
-    msgsrec = msgsrec + 1
-  end
-  c:on("message", novamsg)
-end
+end]]--
